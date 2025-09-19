70989331cf1f005c37a069e5fc206e6b5a4f18e4
1234445666666 <p.k.270607@gmail.com>
2025-09-19 12:01:22 +0300

    fix

--- bin/read.sh
+++ bin/read.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PROJECT_ROOT="$(dirname "$SCRIPT_DIR")"
 NODE_SCRIPT="$PROJECT_ROOT/src/index.ts"
 
-
-npx ts-node "$NODE_SCRIPT" "$@"
+exec npx ts-node "$NODE_SCRIPT" "$@"
