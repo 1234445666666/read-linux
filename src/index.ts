cde766fc4b22ddd93906dee22719c513e74f3474
1234445666666 <p.k.270607@gmail.com>
2025-09-19 09:37:27 +0300

    fix code

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,50 @@
 import { program } from "commander";
-import fs from "fs";
+import chalk from "chalk";
+import ora from "ora";
+import { promises as fs } from "fs";
 import path from "path";
 
-program
-  .name("read")
-  .description("Node.js implementation of cat command")
-  .version("14.0.1")
-  .option("-s, --separator <char>")
-  .argument("[files...]", "files to process")
-  .action((files, options) => {
-    files.forEach((file: string) => {
+async function main() {
+  // const spinner = ora("Loading unicorns").start();
+
+  // setTimeout(() => {
+  //   spinner.color = "yellow";
+  //   spinner.text = "Loading rainbows";
+  // }, 1000);
+  program
+    .name("read")
+    .description("Node.ts implementation of cat command")
+    .version("14.0.1")
+    .option("-b , --babosh", "use babosh")
+    .argument("[files...]", "files to process")
+    .action(async (files, options) => {
       try {
-        const absolutePath = path.resolve(file);
-        console.log("Reading:", absolutePath);
-
-        const data = fs.readFileSync(absolutePath, "utf-8");
-        console.log(data);
-
-        const stats = fs.statSync(absolutePath);
-        console.error(`\nFile size: ${stats.size} bytes`);
+        console.log(options);
+        if (options.babosh) {
+          console.log("BABOSH MODE ACTIVATED!");
+        } else {
+          console.log("Normal mode");
+        }
+        files.forEach((file: string) => {
+          const absolutePath = path.resolve(file);
+          fs.readFile(absolutePath, "utf-8").then((data) => {
+            console.log(chalk.white.italic(data));
+            const lines = data.split("\n");
+            console.log(chalk.green.bold(lines.length, `Lines in ${file}`));
+          });
+          fs.stat(absolutePath)
+            .then((stats) => {
+              console.log(chalk.green.bold(stats.size, `Bytes in ${file}`));
+            })
+            .catch((error) => {
+              console.log(chalk.red.bold("error"));
+            });
+        });
       } catch (error) {
-        console.log("Error");
+        console.log(chalk.red.bold("File not found:", files));
       }
     });
-  });
 
-program.parse();
+  program.parse();
+}
+main();
